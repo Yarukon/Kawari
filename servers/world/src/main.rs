28a28dd78d56b5e0d4ea611e60c5a36566f2b4bb
Yarukon <a2692866561@live.com>
2026-07-27 18:18:40 +0800

    Commit database changes when the client language is set

--- servers/world/src/main.rs
+++ servers/world/src/main.rs
@@ -3840,6 +3840,12 @@ async fn process_packet(
                     }
                     ClientZoneIpcData::SetClientLanguage { language } => {
                         connection.player_data.volatile.client_language = *language;
+
+                        // We need to commit this to the database again since this is read everywhere.
+                        {
+                            let mut database = connection.database.lock();
+                            database.commit_volatile(&connection.player_data);
+                        }
                     }
                     ClientZoneIpcData::RequestCharaInfoFromContentIds { .. } => {
                         tracing::info!(
